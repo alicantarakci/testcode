f480596d0305339ba12c202119f85881b2f08116
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-03-21 14:55:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,119 @@
-// ** Bir müşterinin aşağıdaki bilgileri için değişken oluşturunuz.
+// // Operatörler
 
-// ** Müşteri adı
-// ** Müşteri soyadı
-// ** Müşteri tc kimlik
-// ** Müşteri sipariş toplamı
-// ** Müşteri cinsiyet
-// ** Müşteri adres bilgisi
-// ** Müşteri hobiler
+// let val;
+// const a=10;
+// const b=5;
+// const c=5;
+// let   d=3;
 
-var customerName = 'Çınar';
-var customerLastName = 'Turan';
+// // 1- Aritmetik Operatörler
 
-var fullName = 'Çınar Turan';
-var musteriId = '1322323232';
+// val = a+b;
+// val = a-b;
+// val = a*b;
+// val = a/b;
+// val = a%b;
+// val = d++;
+// val = ++d;
+// val = --d;
+// val = d--;
 
-var total = 205.6;
-var gender = false; // erkek true, kadın false
+// // 2- Atama Operatörleri
 
-// object
-var address ={
-    city : 'Kocaeli',
-    district : 'İzmit',
-    body : 'Ömerağa mah.No:45'
-}
-
-// array
-var hobbies = ['Sinema','Kitap','Spor'];
+// val = a;
+// val +=a; // val = val + a;
+// val -=a; // val = val - a;
+// val *=a; // val = val * a;
+// val /=a; // val = val / a;
+// val %=a; // val = val % a;
 
 
-// ** Aşağıdaki siparişlerin toplamını hesaplayınız.
+// // 3- Karşılaştırma Operatörleri
 
-var order1=Number('100');
-var order2=Number('150');
+// val = a==b;
+// val = b==c;
+// val = b===c; // değer kontrolü & type
+// val = 5 === '5';
+// val = a!=b;
+// val = a!==b;
+// val = a > b;
+// val = b < a;
+// val = a >= b;
+// val = 5 >= 5;
+// val = a <= b;
 
-var totalOrder = order1 + order2;
-console.log(totalOrder);
+// // 4- Mantıksal Operatörler
 
-// ** Aşağıdaki siparişlerin toplamını hesaplayınız.
+//    // && (And)
+//    // true  && true  => true
+//    // true  && false => false
+//    // false && false => false
 
-var order3=Number('100.2');
-var order4=Number('150.5');
+//    val=  (a>b) && (a>c) 
+//    val = (a>b) || (a<c)
+//    val = !(a>b)
 
-var totalOrder2 = order3 + order4;
-console.log(totalOrder2);
 
-// ** Aşağıdaki siparişlerin toplamını tam sayı olarak hesaplayınız.
+//    // || (Or)
+ 
+//    // true  && true  => true
+//    // true  && false => true
+//    // false && false => false
 
-var order4=parseInt('100.2');
-var order5=parseInt('150.5');
+//    // ! (Not)
 
-var totalOrder3 = order4+order5
-console.log(totalOrder3);
+//    // !true  => false
+//    // !false => true
 
-// ** Aşağıda verilen doğum yılına göre yaş hesaplayınız.
+// console.log(val);
+// console.log(typeof val);
 
-const yearOfBirth = 1986;
-// yearOfBirth = 1987;
 
-console.log(new Date().getFullYear()-yearOfBirth);
+// Demo : Operators
 
-// ** Aşağıdaki string ifadenin karakter sayısını bulunuz.
+// 1 - Can ve Ada 'nın boy ve kg bilgilerini alın.
+// 2 - Alınan bilgilere göre kilo indekslerini hesaplayınız.
+//  ** Formül : Kişinin Kilosu / Boy Uzunluğunun Karesi
+// 3 - Hesaplanan indeks bilgisine göre karşılaştırma yapınız.
+// 4 - Aşağıdaki tabloya göre can ve ada hangi gruba giriyor.
 
-let course = 'Modern Javascript Kursu';
+// 0 - 18,4: Zayıf
+// 18,5 - 24,9: Normal
+// 25,0 - 29,9: Fazla Kilolu
+// 30,0 - 34,9: Şişman (Obez)
 
-console.log(course.length);
+let IndexCan;
+let IndexAda;
+
+const kgCan = 60;
+const kgAda = 40;
+
+const heigtCan = 1.70;
+const heigtAda = 1.50;
+
+IndexAda = (kgAda) / (heigtAda*heigtAda);
+IndexCan = (kgCan) / (heigtCan*heigtCan);
+
+console.log(IndexAda.toFixed(2),IndexCan.toFixed(3));
+
+let adaHigherIndex = IndexAda>IndexCan;
+let canHigherIndex = IndexCan>IndexAda;
+
+console.log("Ada'nın kilo indeksi can'ın kilo indeksinden daha büyük : "+adaHigherIndex);
+
+console.log("Can'ın kilo indeksi ada'nın kilo indeksinden daha büyük : "+canHigherIndex);
+
+let AdaZayif = (IndexAda>=0) && (IndexAda<=18.4);
+let AdaNormal = (IndexAda>=18.5) && (IndexAda<=24.9);
+let AdaKilolu = (IndexAda>=25) && (IndexAda<=29.9);
+let AdaSisman = (IndexAda>=30) && (IndexAda<=34.9);
+
+console.log("Ada zayıf :"+AdaZayif);
+console.log("Ada'nın kilosu normal : "+AdaNormal);
+console.log("Ada kilolu mu : "+AdaKilolu);
+console.log("Ada şişman mı : "+AdaSisman);
+
+
 
 
 
