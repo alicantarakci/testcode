31a74fa62845bd57b13e0237e48ca4200a54b7e4
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-03-23 13:34:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,31 @@
-// ** Şimdiki tarihin gün ay ve yıl bilgisini yazdırınız.
+// Numbers
 
-var dt = new Date();
+let val;
 
-console.log(dt);
-console.log(dt.getMonth()+1);
-console.log(dt.getDate());
-console.log(dt.getFullYear());
+// val = Number('10');
+// val = parseInt('10');
+// val = parseFloat('10.5');
+// val = parseInt('a10a');
+// val = isNaN('10');
+// val = isNaN('a10');
 
-// ** Tarih ve saat bilgisini içeren bir Date objesi oluşturunuz.
-var dtA = new Date('8/24/2010 14:50:10');
-var dtB = new Date(2010,7,24,14,50,10)
+// var num = 10.12456789;
+// val = num.toPrecision(6);
+// val = num.toFixed(2);
 
-console.log(dtA);
-console.log(dtB);
+val = Math.PI;
+val = Math.round(2.4);
+val = Math.round(2.7);
+val = Math.ceil(2.4);
+val = Math.ceil(2.6);
+val = Math.floor(2.4);
+val = Math.floor(2.7);
+val = Math.sqrt(64);
+val = Math.pow(2,4);
+val = Math.abs(-100);
+val = Math.min(1,2,3,2,4,9);
+val = Math.max(4,5,6,4,98,7);
+val = Math.floor(Math.random()*100+1);
 
-// 1/1/1990 tarihinden bir gün öncesini gösteriniz. 
-var dtC = new Date('1/1/1990');
-var dayOfMonth = dtC.getDate();
-dtC.setDate(dayOfMonth-1);
-console.log(dtC);
-
-// iki tarih arasındaki geçen zamanı bulunuz.
-var start = new Date('1/1/1990');
-var end = new Date('2/10/1992');
-
-var milisecond = end - start;
-var saniye = milisecond / 1000;
-var dakika = saniye / 60;
-var saat = dakika / 60;
-var gun = saat / 24;
-
-console.log('milisecond :'+milisecond)
-console.log('saniye : '+ saniye);
-console.log('dakika :'+ dakika);
-console.log('saat :' + saat);
-console.log('gun : '+gun);
-
-// Her yıl mayıs ayının 2.haftası pazar günü kutlanan anneler günü 2019 yılında ne zaman kutlanacaktır ?
-
-var annelerGunu = new Date();
-annelerGunu.setHours(0,0,0,0);
-annelerGunu.setFullYear(2019);
-annelerGunu.setDate(1);
-annelerGunu.setMonth(4);
-
-while(annelerGunu.getDay() != 0){
-    annelerGunu.setDate(annelerGunu.getDate()+1)
-}
-annelerGunu.setDate(annelerGunu.getDate()+7);
-console.log(annelerGunu);
-
-// ** Yaş hesaplama nasıl yapılır ?
-
-var birthday = new Date('8/1/1985');
-var ageDifMs = Date.now() - birthday.getTime();
-var ageDate = new Date(ageDifMs);
-
-console.log(ageDate.getFullYear() - 1970);
-// console.log(birthday.getTime());
-// console.log(Date.now())
-
-
-var kurs = "Modern Javascript Dersleri: Baştan Sona Javascript Programlama";
-var result = kurs.lastIndexOf("Javascript"); // result: 7
-
-console.log(result)
+console.log(val);
+console.log(typeof val);
