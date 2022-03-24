43ca72713c3848ea86a218877ce534960c4777a2
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-03-24 18:26:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-// Numbers
+// Strings
+
+const firstName = 'Sadık';
+const lastName = "Turan";
+let hobbies = 'sinema spor kitap yazılım';
+const age=35;
 
 let val;
 
-// val = Number('10');
-// val = parseInt('10');
-// val = parseFloat('10.5');
-// val = parseInt('a10a');
-// val = isNaN('10');
-// val = isNaN('a10');
+// string concatenation
 
-// var num = 10.12456789;
-// val = num.toPrecision(6);
-// val = num.toFixed(2);
+val = firstName +" "+ lastName;
+val = 'Sadık';
+val +=' Turan';
 
-val = Math.PI;
-val = Math.round(2.4);
-val = Math.round(2.7);
-val = Math.ceil(2.4);
-val = Math.ceil(2.6);
-val = Math.floor(2.4);
-val = Math.floor(2.7);
-val = Math.sqrt(64);
-val = Math.pow(2,4);
-val = Math.abs(-100);
-val = Math.min(1,2,3,2,4,9);
-val = Math.max(4,5,6,4,98,7);
-val = Math.floor(Math.random()*100+1);
+val = 'Benim adım '+firstName+' '+ lastName+ ' ve yaşım ' + age + ' izmit\'te yaşıyorum.';
+
+//string concat
+val = firstName.concat(' ',lastName);
+
+// string uppercase - lowercase
+val = val.toUpperCase();
+val = val.toLowerCase();
+
+// string replace
+val = '  '+val.replace('sadik','çınar')+'     ';
+
+// trim
+val = val.trim();
+
+// substring 
+// val = val.substring(3,8);
+
+// slice
+// val = val.slice(6); 
+
+//val = val.indexOf('x');
+
+//val = val[1];
+
+//string length
+//val = val.length;
+
+val =hobbies.split(' ')
 
 console.log(val);
 console.log(typeof val);
