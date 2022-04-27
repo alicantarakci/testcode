986c19fdd8289a675e4991cc49b232b0d566dc46
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-04-27 16:20:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
-let simdi = new Date(); // şimdiki tarih - saat
+// 1- "Elma,Armut,Muz,Çilek" elemanlarına sahip bir dizi oluşturunuz.
 
-// Get Methods
-sonuc = simdi;
-sonuc = simdi.getDate();    // gün
-sonuc = simdi.getDay();     // 0: pazar 6: cumartesi
-sonuc = simdi.getFullYear(); // yıl
-sonuc = simdi.getHours();   // saat
-sonuc = simdi.getTime();
+let meyveler = ["Elma","Armut","Muz","Çilek"];
 
-// Set Methods
-// simdi.setFullYear(2025);
-simdi.setMonth(7);          // 0: Ocak
-simdi.setDate(15);
+// 2- Dizi kaç elemanlıdır?
 
-sonuc = simdi;
+console.log(meyveler.length);
 
-let dogumTarihi = new Date(1990, 5, 15);
+// 3- Dizinin ilk ve son elemanı nedir?
+console.log(meyveler[0]);   // Elma
+console.log(meyveler[meyveler.length - 1]);   // Çilek
 
-sonuc = simdi.getFullYear() - dogumTarihi.getFullYear();
+// 4- Elma dizinin bir elemanımıdır?
+console.log(meyveler.includes("Elma"));
+console.log(meyveler.indexOf("Elma"));
 
-let milisecond = simdi - dogumTarihi;
-let saniye = milisecond / 1000;     // saniye
-let dakika = saniye / 60;
-let saat = dakika / 60;
-let gun = saat / 24;
+// 5- Kiraz meyvesini listenin sonuna ekleyiniz.
 
-sonuc = gun;
+// meyveler[meyveler.length] = "Kiraz";
+meyveler.push("Kiraz");
 
-console.log(sonuc);
-console.log(typeof sonuc);
+// 6- Dizinin son 2 elemanını siliniz.
 
+// meyveler.pop();
+// meyveler.pop();
+
+// meyveler.splice(meyveler.length - 2, 2);
+
+// 7- Aşağıdaki bilgileri dizi içerisinde saklayınız ve her öğrencinin yaşını ve not ortalamasını hesaplayınız.
+    /*
+        Öğrenci 1: Yiğit Bilgi 2010 (70,60,80)
+        Öğrenci 2: Ada Bilgi 2012   (80,80,90)
+        Öğrenci 3: Ahmet  2009 (60,60,70)
+    */
+
+let ogr1 = [
+    "Yiğit",
+    "Bilgi",
+    2010,
+    [70,60,80]
+];
+
+let ogr2 = [
+    "Ada",
+    "Bilgi",
+    2012,
+    [80,80,90]
+];
+
+let ogr3 = [
+    "Ahmet",
+    "Turan",
+    2009,
+    [60,60,70]
+];
+
+let ogrenciler = [ogr1, ogr2, ogr3];
+
+let yigit_yas =  new Date().getFullYear() - ogrenciler[0][2];
+let ada_yas =  new Date().getFullYear() - ogrenciler[1][2];
+let ahmet_yas =  new Date().getFullYear() - ogrenciler[2][2];
+
+let yigit_not = (ogrenciler[0][3][0] + ogrenciler[0][3][1] + ogrenciler[0][3][2]) / 3;
+let ada_not = (ogrenciler[1][3][0] + ogrenciler[1][3][1] + ogrenciler[1][3][2]) / 3;
+let ahmet_not = (ogrenciler[2][3][0] + ogrenciler[2][3][1] + ogrenciler[2][3][2]) / 3;
+
+console.log(yigit_not.toFixed(1), ada_not.toFixed(1), ahmet_not.toFixed(1));
