a56ad1fcc08e8ab763cdf306d5ef0d8b09df7f9d
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-05-07 23:43:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,17 +7,13 @@ sonuc = url.startsWith("https");
 if (sonuc) {
     console.log("evet başlıyor");
 }
-
 if (kursAdi.indexOf("Eğitimi") > -1) {
     console.log("evet var");
 } else {
     console.log("hayır yok");
 }
-
 kursAdi = kursAdi.toLowerCase();
 kursAdi = kursAdi.replaceAll(" ","-");
 kursAdi = kursAdi.replace("ş","s").replace("ı","i");
-
 sonuc = `${url}/${kursAdi}`;
-
 console.log(sonuc);
