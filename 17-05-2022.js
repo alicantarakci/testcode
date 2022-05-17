164f10ecd8b717816bcdfe0dbda37e2934271bc6
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-05-17 16:20:39 +0300

    Add files via upload

---
+++ 17-05-2022.js
@@ -0,0 +1,16 @@
+let films =["Hızlı Ve Öfkeli","Arınma Gecesi","Recep İvedik","Aykut Enişte"];
+films.push ("inek Şaban");
+console.log(films);
+films.unshift ("Underground");
+console.log(films);
+films.pop ()
+console.log(films);
+films.shift ()
+console.log(films)
+var indeks = films.indexOf("Arınma Gecesi");
+console.log(indeks)
+console.log(films.includes('Hızlı Ve Öfkeli'));
+console.log(films.length);
+films.sort();
+console.log(films);
+console.log(films.splice(2));
