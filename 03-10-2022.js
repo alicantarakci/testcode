5be9039e8017212b59c9d66797596393e66a7ae8
Ali Can Tarakci <71080915+alicantarakci@users.noreply.github.com>
2022-10-03 18:24:11 +0300

    Add files via upload

--- 03-10-2022.js
+++ 03-10-2022.js
@@ -59,3 +59,95 @@ if(paralar[i] % 5 == 0){
 }
 }
 console.log('\n');
+let notebooks = ["Acer","Monster","Toshiba","GameGaraj","Dell","Asus"];
+let accessorys = ["Mause","Keyboard","Headphone","Monitor"];
+let accessorysModels = ["Logitech","Rampage","Steelseries","Asus"];
+let setups =[...notebooks,...accessorys,...accessorysModels];
+//ferrainin suv modellerini dönder
+cars.Ferrari.Suv.forEach(function(element,index) {
+  console.log(index +'Ferrari modeli'+ element.Model+ 'Fiyatı ' + element.Price + 'Motor Gücü'+element.HorsePower);
+});
+//toString() {
+  //  return `{Modeli: "${this.Ferrari.Suv.Model}", Fiyatı: ${this.Ferrari.Suv.Price}, Gücü: ${this.Ferrari.Suv.HorsePower} }`;
+  //}
+  cars.Ferrari.Suv.forEach(function(elem, index) {
+    console.log(`${index} 'Ferrari modeli' ${elem.Model} 'Fiyatı ' ${elem.Price}  ${elem.HorsePower} 'Motor Gücü'`);
+  });
+  //do while ile ekrana yazdırma mersedes hachback i değişkeni tanımlanıcak 0 dan 
+  //tanımlacnacak i<=1 olduğu süerece çalışacak i üzerinden mersedesi kullanmak
+ var i = 0;
+do {
+    console.log(cars.Mersedes.Hachback[i].Model + ' Mersedes modeli ' +cars.Mersedes.Hachback[i].Price +  ' Fiyatı ' + cars.Mersedes.Hachback[i].HorsePower + ' Motor Gücü ' );
+    i++;
+}
+while (i<=2);
+// yeni bir mersedes modeli oluştur marka model yazıyorum diğer diziyle birleştiriyorum
+NewMersedes={Model:"fjghmdsfgflgjh",Price:"23124$",HorsePower:"921bg"};
+cars.Mersedes.Hachback.push(NewMersedes);
+console.log(cars.Mersedes.Hachback);
+var ü = 1;
+while  (ü < 10){
+    console.log(ü);
+    ü++;
+}
+console.log('\n');
+var b = 1 ;
+do {
+    console.log(b);
+    b++;
+}
+ while (j > 10);
+
+var j=1;
+while (j < 10){
+    console.log(j);
+    j++;
+}
+console.log('\n');
+for ( var y = 1; y < 10; y++){
+    console.log(y);
+}
+console.log('\n');
+for ( var s = 1; s < 10; s++){
+    if (s==5){
+        break;
+    }
+    console.log(s);
+}
+console.log('\n');
+for(var i=1;i<=10;i++)
+	{
+		console.log("EN BÜYÜK FENERBAHÇE");
+	}
+console.log('\n');
+console.log(setups);
+console.log('\n');
+console.log(setups[10]);
+console.log('\n');
+console.log(setups.length);
+setups.push ("Acer");
+console.log(setups);
+console.log('\n');
+setups.sort();
+console.log(setups);
+setups.forEach(function(hello){
+    console.log(`En iyi bilgisayarlar ve ekipmanları ${hello}`);});
+  console.log('\n');
+  var Setupslist = setups.entries();
+  for (let e of Setupslist) {
+    console.log(e);
+  };
+  let urunler = [
+    {
+        "urun_adi": "samsung s22",
+        "urun_fiyat": 13000
+    },
+    {
+        "urun_adi": "samsung s23",
+        "urun_fiyat": 15000
+    }
+]
+
+sonuc = urunler[0].urun_adi;
+
+console.log(sonuc);
